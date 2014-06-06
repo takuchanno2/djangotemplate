77f95a60944c4375440d367f15b042b93e44c528
takuchan No.2 <taku_yosi@hotmail.com>
2014-06-07 01:22:19 +0900

    manag.pyを修正

--- djangotemplate/manage.py
+++ djangotemplate/manage.py
@@ -6,14 +6,8 @@ import sys
 import django.conf
 
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
     from django.core.management import execute_from_command_line
 
-    # プロジェクトのプロパティで渡される"Settings Module"の値は無視
-    argv = sys.argv
-    if "--settings" in argv:
-        idx = argv.index("--settings")
-        del argv[idx:(idx+2)]
-
-    execute_from_command_line(argv)
+    execute_from_command_line(sys.argv)
